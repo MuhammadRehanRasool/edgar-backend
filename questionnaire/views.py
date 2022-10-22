83f63136f3ceb4b49b97801bd36a529692289a38
abar ahmed <abrarahmed2185@gmail.com>
2022-10-23 01:33:37 +0500

    Stripe

--- questionnaire/views.py
+++ questionnaire/views.py
@@ -8,37 +8,63 @@ from rest_framework.decorators import api_view, permission_classes
 from django.shortcuts import redirect
 # Stripe import
 import stripe
+import json
 
 stripe.api_key = 'sk_test_51LvflpEzOnSWJxP5wAUZaz0GwCcFO4aawZd9G91v8wUSiDV2NYC2y7FiZpkMnsWLCxFJcPqpGz7vwfU9Z4OrC4R0009X88pIFs'
+
+
+def getProductIdFromStripe(product_name):
+    product_search = stripe.Product.search(
+        query="name:'"+product_name+"' ",
+    )
+    product_id = None
+    if len(product_search["data"]) != 0:
+        product_id = product_search["data"][0]["id"]
+    else:
+        product_id = stripe.Product.create(name=product_name).id
+    return product_id
+
+
+def getPriceIdFromStripe(original_amount, product_id):
+    price_search = stripe.Price.search(
+        query="product:'"+product_id+"' ",
+    )
+    price_id = None
+    for temp in price_search:
+        if int(original_amount) == int(temp["unit_amount"]):
+            price_id = temp["id"]
+    if price_id is None:
+        price_id = stripe.Price.create(
+            unit_amount=original_amount,
+            currency="usd",
+            product=product_id,
+        ).id
+    return price_id
 
 
 @api_view(['GET', 'POST'])
 @permission_classes([])
 def testing(request):
     if request.method == "GET":
-        res = stripe.Product.search(
-            query="name:'Maths' ",
-        )
-
-        print(res)
-        return JsonResponse({'message': res}, status=status.HTTP_200_OK)
+        price_id = getPriceIdFromStripe(70, "prod_Mf890XCCTDw51d")
+        print(price_id)
+        return JsonResponse({'message': ""}, status=status.HTTP_200_OK)
     if request.method == "POST":
         coming_data = JSONParser().parse(request)
-        amount = coming_data['amount']*100
+        amount = int(coming_data['amount'])*100
         product_name = coming_data['product']
         try:
-            product = stripe.Product.create(name=product_name)
-            pr_price = stripe.Price.create(
-                unit_amount=amount,
-                currency="usd",
-                product=product.id,
-            )
-
+            product_id = getProductIdFromStripe(product_name)
+            pr_price = getPriceIdFromStripe(amount, product_id)
+            # print("Amount", amount)
+            # print("product_name", product_name)
+            # print("product_id", product_id)
+            # print("pr_price", pr_price)
+            # print("\n\n\khatam tata")
             checkout_session = stripe.checkout.Session.create(
                 line_items=[
                     {
-                        # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                        'price': pr_price.id,
+                        'price': pr_price,
                         'quantity': 1,
                     },
 
@@ -48,7 +74,7 @@ def testing(request):
                 # '?success=true&session_id={CHECKOUT_SESSION_ID}',
                 # cancel_url='localhost:3000/' + '?canceled=true',
                 success_url='https://github.com/',
-                cancel_url='https://github.com/abrar-11',
+                cancel_url='https://github.com10lk236o566t,
             )
         except Exception as e:
             return JsonResponse({'Error':  str(e)}, status=500)
